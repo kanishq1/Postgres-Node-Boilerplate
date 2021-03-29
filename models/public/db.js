e8a5e3852957517a8f5b3ac02747c0cc97498bef
kanishq1 <59061353+kanishq1@users.noreply.github.com>
2021-03-29 22:14:41 +0530

    Initial commit

---
+++ models/public/db.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const Sequelize = require('sequelize');
+var config = require("../../config/config");
+
+var env = config.db.env;
+
+const sequelize = new Sequelize(env.DATABASE_NAME, env.DATABASE_USERNAME, env.DATABASE_PASSWORD, {
+  host: env.DATABASE_HOST,
+  port: env.DATABASE_PORT,
+  dialect: env.DATABASE_DIALECT,
+  schema: env.SCHEMA,
+  define: {
+    underscored: true
+  },
+  // socketPath : env.SOCKET_PATH,
+  // dialectOptions: env.DIALECT_OPTIONS
+//  logging: false
+});
+
+// Connect all the models/tables in the database to a db object,
+//so everything is accessible via one object
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.Op = Sequelize.Op;
+
+//Models/tables
+db.login = require('./login.js')(sequelize, Sequelize);
+db.permissions = require('./permissions.js')(sequelize, Sequelize);
+db.policy = require('./policy.js')(sequelize, Sequelize);
+db.kv = require('./kvStore.js')(sequelize, Sequelize);
+
+//Relations
+// db.profile.belongsTo(db.login, {onDelete: "CASCADE"});
+
+module.exports = db;
